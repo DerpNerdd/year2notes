c5dd9de37d463e776faadb7ba99c7ceee0177ac6
Alan Sanchez <sanchez1.alan1@gmail.com>
2024-08-01 15:13:29 -0700

    aaaaaaa

---
+++ Unit1/ArrayMethodsPractice/ternaries.js
@@ -0,0 +1,11 @@
+let x = 25
+let y = 59
+
+if(x <= y){
+    console.log("x is less than or equal to y")
+}
+else{
+    console.log("x is greater than y")
+}
+
+{x <= y ? console.log("x is less than or equal to y") : console.log("x is greater than y")}
