aad8200e6fcaee3ece7d32676a27f4926889bdbc
DerpNerd <sanchez1.alan1@gmail.com>
2024-08-29 13:29:15 -0700

    i HATE noelle
    chigga

---
+++ NodeJS/tutorials/event-loop-examples/01-read-file.js
@@ -0,0 +1,13 @@
+const {readFile, writeFile} = require('fs')
+
+console.log('Started the first task')
+//Check your file path
+readFile('../content/first.txt', 'utf8', (err, result) => {
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(result)
+    console.log('Finished the first task')
+})
+console.log('Starting the second task')
