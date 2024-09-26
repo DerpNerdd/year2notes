f40ab7f7d3a15c80733f3210cdbd2e6bedede42a
DerpNerd <sanchez1.alan1@gmail.com>
2024-09-26 14:51:33 -0700

    Update 04-params-query.js

--- Express/04-params-query.js
+++ Express/04-params-query.js
@@ -19,14 +19,41 @@ app.get('/api/products', (req, res) => {
 //This is how you set up params for the data query
 
 app.get('/api/products/:productID', (req, res) => {
-    console.log(req.params)
+    console.log(req.query)
     const {productID} = req.params
     const singleProduct = products.find((product) => product.id === Number(productID))
 
     if(!singleProduct){
         return res.status(404).send('Product Does Not Exist')
+    } else if(req.query.Life == '42'){
+        return res.status(200).send('The answer to life, the universe, and everything is 42')
     }
     return res.json(singleProduct)
+})
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+    console.log(req.params)
+    console.log(Number(req.params.productID))
+    res.send('This product has been reviewd by a person: ITS AMAZING IM BAUTAKUMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM')
+})
+
+app.get('/api/v1/query', (req, res) => {
+    console.log(req.query)
+    const {search, limit} = req.query
+    let sortedProducts = [...products]
+    if(search){
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search)
+        })
+    }
+    if(limit){
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if(sortedProducts.length < 1){
+        //res.status(200).send('No products matched your search')
+        return res.status(200).json({success: true, data: []})
+    }
+    res.status(200).json(sortedProducts)
 })
 
 app.listen(3000, () => {
