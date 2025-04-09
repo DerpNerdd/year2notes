c93c25a6345f606ba9b72a966aced5fd68bd89d9
Alan Sanchez <sanchez1.alan1@gmail.com>
2025-04-09 15:11:32 -0700

    TypeScript

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,5 +1,12 @@
+"use strict";
 var ts = "pmo sm ngl icl 🥀⛓";
 var age = 17;
 var Name = "Alan";
 var alive = false;
-var greeting = "Welcome to Franchez Inc.";
+let greeting = "Welcome to Franchez Inc.";
+function add(n1, n2) {
+    return n1 + n2;
+}
+console.log(add(1312, 313213));
+let result;
+result = 213;
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -5,3 +5,13 @@ var Name:string = "Alan";
 var alive:boolean = false;
 
 let greeting:string = "Welcome to Franchez Inc."
+
+function add (n1: number, n2: number) {
+    return n1 + n2;
+}
+
+console.log(add(1312, 313213))
+
+let result: string | number;
+
+result = 213
